0b05c800956ba2c133f54a890a72549154f6869c
jinyalin <jinyalin@baidu.com>
2017-03-19 17:24:49 +0800

    add iris data classifier

--- NeuralNetwork/mnist_Demo2.py
+++ NeuralNetwork/mnist_Demo2.py
@@ -4,7 +4,7 @@ import mnist_network2
 training_data, validation_data, test_data = \
     mnist_loader.load_data_wrapper()
 
-net = mnist_network2.Network([784, 30, 10], cost=mnist_network2.CrossEntropyCost)
+net = mnist_network2.Network([784, 30, 30, 10], cost=mnist_network2.CrossEntropyCost)
 net.SGD(training_data, 30, 10, 0.5,
         lmbda=5.0,
         evaluation_data=validation_data,
