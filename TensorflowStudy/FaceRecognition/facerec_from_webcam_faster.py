9d0f6faa3dfdd04608ce36b38a1cfe081b215166
jinyalin <jinyalin@baidu.com>
2017-06-22 20:20:50 +0800

    add some tensorflow examples

--- TensorflowStudy/FaceRecognition/facerec_from_webcam_faster.py
+++ TensorflowStudy/FaceRecognition/facerec_from_webcam_faster.py
@@ -39,7 +39,7 @@ while True:
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            match = face_recognition.compare_faces([yalin_face_encoding], face_encoding)
+            match = face_recognition.compare_faces([yalin_face_encoding], face_encoding, tolerance=0.4)
             name = "Unknown"
 
             if match[0]:
---
+++ TensorflowStudy/TensorFlowExamples/2_BasicModels/nearest_neighbor.py
@@ -0,0 +1,39 @@
+"""
+A nearest neighbor learning algorithm example using TensorFlow library.
+This example is using the MNIST database of handwritten digits
+(http://yann.lecun.com/exdb/mnist/)
+"""
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("../../mnist/MNIST-data/", one_hot=True)
+
+Xtr, Ytr = mnist.train.next_batch(5000)
+Xte, Yte = mnist.test.next_batch(200)
+
+xtr = tf.placeholder("float", [None, 784])
+xte = tf.placeholder("float", [784])
+
+distance = tf.reduce_sum(tf.abs(tf.add(xtr, tf.negative(xte))), reduction_indices=1)
+
+pred = tf.arg_min(distance, 0)
+
+accuracy = 0.
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for i in range(len(Xte)):
+        nn_index = sess.run(pred, feed_dict={xtr: Xtr, xte: Xte[i]})
+        print("Test", i, "Prediction:", np.argmax(Ytr[nn_index]),
+              "True Class:", np.argmax(Yte[i]))
+        if np.argmax(Ytr[nn_index]) == np.argmax(Yte[i]):
+            accuracy += 1. / len(Xte)
+
+    print("Done!")
+    print("Accuracy:", accuracy)
