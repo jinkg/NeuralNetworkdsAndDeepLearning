dd415b61b2d424282fdd8b5dc23d639587e1ad38
jinyalin <jinyalin@baidu.com>
2017-07-05 17:27:50 +0800

    add k-means for nearest_neighbor

--- TensorflowStudy/TensorFlowExamples/2_BasicModels/nearest_neighbor.py
+++ TensorflowStudy/TensorFlowExamples/2_BasicModels/nearest_neighbor.py
@@ -6,6 +6,7 @@ This example is using the MNIST database of handwritten digits
 
 import numpy as np
 import tensorflow as tf
+import operator
 
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -18,6 +19,30 @@ xtr = tf.placeholder("float", [None, 784])
 xte = tf.placeholder("float", [784])
 
 distance = tf.reduce_sum(tf.abs(tf.add(xtr, tf.negative(xte))), reduction_indices=1)
+sortedDistIndices = tf.nn.top_k(-distance, k=3).indices
+minDistance = tf.gather(distance, sortedDistIndices)
+
+# K-means
+k_accuracy = 0.
+with tf.Session() as sess:
+    for i in range(len(Xte)):
+
+        indices = sess.run(sortedDistIndices, feed_dict={xtr: Xtr, xte: Xte[i]})
+
+        classCount = {}
+        for index in range(len(indices)):
+            # trueClass = np.argmax(Yte[i])
+            predictionClass = np.argmax(Ytr[indices[index]])
+            classCount[predictionClass] = classCount.get(predictionClass, 0) + 1
+            print("Result", index, "Prediction:", predictionClass,
+                  "True Class:", np.argmax(Yte[i]))
+
+        pred = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)[0][0]
+        if pred == np.argmax(Yte[i]):
+            k_accuracy += 1. / len(Xte)
+
+    print("Done!")
+    print("K_Accuracy:", k_accuracy)
 
 pred = tf.arg_min(distance, 0)
 
@@ -37,3 +62,5 @@ with tf.Session() as sess:
 
     print("Done!")
     print("Accuracy:", accuracy)
+
+print("Compare:", "k_accuracy", k_accuracy, "accuracy", accuracy)
